637d284a849743f824afb4901f3c6df402eaab70
Lucas Alves Rodrigues <contato.lucasalv@gmail.com>
2022-05-09 22:14:09 -0300

    chore: adjust code for deploy on heroku

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.get("/", (req, res) => {
+	res.send("Hello World!")
+})
+
 app.listen(process.env.PORT || 5000, () => {
 	console.log(`Server is running on port ${process.env.PORT || 5000}`)
 })
