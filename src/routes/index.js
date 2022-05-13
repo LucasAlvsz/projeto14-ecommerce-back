55896d8064a3becea5f0735dc10ccfe0d3731666
Lucas Alves Rodrigues <contato.lucasalv@gmail.com>
2022-05-13 15:22:33 -0300

    chore: finish all merges

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from "express"
 
 import userRouter from "./authRoutes.js"
+import productRouter from "./productRoutes.js"
 
 const router = Router()
 
 router.use(userRouter)
+router.use(productRouter)
 
 export default router
