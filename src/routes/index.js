1ff30d42987143d196bc5c1add62c2a3d17c0afd
Weslen Mendes <weslenmends@gmail.com>
2022-05-15 13:27:19 -0300

    feat: implemented checkout service

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ import { Router } from "express"
 import userRouter from "./authRoutes.js"
 import productsRouter from "./productsRoutes.js"
 import cartRouter from "./cartRoutes.js"
+import checkoutRouter from "./checkoutRoutes.js"
 
 const router = Router()
 
 router.use(userRouter)
 router.use(productsRouter)
 router.use(cartRouter)
+router.use(checkoutRouter)
 
 export default router
